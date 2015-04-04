41921e87e77af5a617f6ef3300436301aae1cfed
carsonhu <carsonhu@ucla.edu>
2015-04-04 15:45:45 -0700

    he can now REALLY jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public float speed = 1000;
+	public float speed = 200;
 	public float xVelocity;
 	public float yVelocity;
+	public bool canJump = true;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,18 +13,18 @@ public class PlayerController : MonoBehaviour {
 	//GetCompononet<Rigidbody>().AddForce(asdf)
 	// Update is called once per frame
 	void FixedUpdate () {
+
 		float horizontal = Input.GetAxis ("Horizontal");
-		float vertical;
-		if (yVelocity == 0) {
-			vertical = Input.GetAxis ("Vertical");
-		} else {
-			vertical = 0;
+		//float vertical;
+		if (Input.GetKeyDown ("space") && canJump == true) {
+			canJump = false;
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
 		}
-		xVelocity = horizontal;
-		yVelocity = vertical;
+
+
 		
-		Vector3 movement = new Vector3 (horizontal, vertical, 0);
-		
+		Vector3 movement = new Vector3 (horizontal, 0, 0);
+
 		
 		GetComponent<Rigidbody2D> ().AddForce (movement * speed * Time.deltaTime);
 		//xVelocity = horizontal * Time.deltaTime * speed;
@@ -31,15 +32,18 @@ public class PlayerController : MonoBehaviour {
 		//Vector3 position = new Vector3 (horizontal * Time.deltaTime * speed, vertical * Time.deltaTime * speed, 0);
 		//transform.position = transform.position + position;
 		
-		if (transform.position.x < -30) {
-			transform.position = new Vector3(10, 0, 0);
+		if (transform.position.y < -10) {
+			print ("YOU LOSE YOOOOOO");
+			Destroy (this);
 		}
-		
+
 
 	}
 	
-	//void onTriggerEnter(Collider other){
-	//	if (other.gameObject.tag == "")
-	//		;
-	//}
+	void onTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Ground"){
+			canJump = true;
+		}
+	//		
+	}
 }
